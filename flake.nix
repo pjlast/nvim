19ffb74f6d36b2bd032d1cf5c64d1208b016b9ea
Petri-Johan Last <petri.last@gmail.com>
2025-06-28 15:54:59 +0200

    Update nix flake for x86_64

--- flake.nix
+++ flake.nix
@@ -6,6 +6,12 @@
   };
 
   outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [
+        nixpkgs.legacyPackages.x86_64-linux.lua-language-server
+      ];
+    };
+
     devShells.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
       buildInputs = [
         nixpkgs.legacyPackages.aarch64-darwin.lua-language-server
